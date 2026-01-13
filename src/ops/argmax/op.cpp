d616f138313a409f1884ae0204a4ba7b449e7be1
niugrandpa <2120280097@qq.com>
2026-01-13 22:53:00 +0800

    2.1 finished

--- src/ops/argmax/op.cpp
+++ src/ops/argmax/op.cpp
@@ -1,7 +1,16 @@
 #include "op.hpp"
 
+#include "../../core/llaisys_core.hpp"
+#include "../../utils.hpp"
+
+#include "cpu/argmax_cpu.hpp"
 namespace llaisys::ops {
 void argmax(tensor_t max_idx, tensor_t max_val, tensor_t vals) {
-    TO_BE_IMPLEMENTED();
+    CHECK_SAME_DEVICE(max_idx,max_val,vals);
+    CHECK_SAME_DTYPE(max_val->dtype(), vals->dtype());
+    ASSERT(max_idx->isContiguous() && max_val->isContiguous() && vals->isContiguous(), "Add: all tensors must be contiguous.");
+    if (vals->deviceType() == LLAISYS_DEVICE_CPU) {
+        return cpu::argmax(max_idx->data(),max_val->data(),vals->data(),vals->dtype(),vals->numel());
+    }
 }
 } // namespace llaisys::ops
